c2e26af1ca582ae2e43677389be1c3c1af68be81
xiaollu <luxlong99@gmail.com>
2022-11-04 23:30:51 +0100

    add scheduler

--- train.py
+++ train.py
@@ -1,15 +1,14 @@
 import os
 import argparse
-from pickle import FALSE
 import pprint
 
 import random
-from xmlrpc.client import boolean
 import numpy as np
 import torch
 import torch.optim as optim
 from torch.utils.data import DataLoader
 from torchvision import transforms
+from torch.optim.lr_scheduler import ReduceLROnPlateau
 
 from lib.core.function import train
 from lib.core.function import validate
@@ -20,6 +19,8 @@ from lib.utils.utils import save_checkpoint
 
 from lib.models.Unet import Res50_UNet
 from lib.dataset.SwissImage import SwissImage
+from lib.utils.utils import MyRandomRotation90
+
 
 # fix random seeds for reproducibility
 seed = 1
@@ -46,11 +47,19 @@ def parse_args():
                         type=float)      
     parser.add_argument('--bs',
                         help='batch size',
-                        default=64,
+                        default=16,
+                        type=int)
+    parser.add_argument('--lr_factor',
+                        help='scheduler_decay_rate',
+                        default=0.8,
+                        type=int)
+    parser.add_argument('--patience',
+                        help='scheduler_patience',
+                        default=5,
                         type=int)
     parser.add_argument('--out_dir',
                         help='directory to save outputs',
-                        default='out',
+                        default='out/train',
                         type=str)
     parser.add_argument('--backbone',
                         help='backbone of encoder',
@@ -58,7 +67,7 @@ def parse_args():
                         type=str)
     parser.add_argument('--frequent',
                         help='frequency of logging',
-                        default=500,
+                        default=100,
                         type=int)
     parser.add_argument('--gpus',
                         help='which gpu(s) to use',
@@ -79,7 +88,7 @@ def parse_args():
                         type=bool)
     parser.add_argument('--tune',
                         help='is tunning?',
-                        default=False,
+                        default=True,
                         type=bool)
     args = parser.parse_args()
     
@@ -87,24 +96,20 @@ def parse_args():
 
 def main():
     args = parse_args()
-    
-    logger, tb_logger = create_logger(args.out_dir, phase='train', create_tf_logs=True)
-    
+
+    logger, tb_logger, folder_name = create_logger(args.out_dir, phase='train', create_tf_logs=True)
     logger.info(pprint.pformat(args))
     
     if args.backbone == 'resnet50':
-        model = Res50_UNet(num_classes=16)
+        model = Res50_UNet(num_classes=10)
     
-    if tb_logger:
-        writer_dict = {
+    writer_dict = {
             'logger': tb_logger,
             'train_global_steps': 0,
             'valid_global_steps': 0,
             'vis_global_steps': 0,
-        }
-    else:
-        writer_dict = None
-    
+            }
+
     if len(args.gpus) == 0:
         gpus = []
     else:
@@ -113,25 +118,40 @@ def main():
     # Define loss function (criterion) and optimizer  
     if len(gpus) > 0:
         model = torch.nn.DataParallel(model, device_ids=gpus).cuda()
-        criterion = CrossEntropy2D().cuda()
+        criterion = CrossEntropy2D(ignore_index=0).cuda()
     else:
-        criterion = CrossEntropy2D()
+        criterion = CrossEntropy2D(ignore_index=0)
     
     optimizer = optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.wd)
+    lr_scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=args.lr_factor, patience=args.patience)
     
     # Create training and validation datasets
     if args.tune:
-        train_csv = '/data/xiaolong/master_thesis/data/subset/train_subset.csv'
-        val_csv = '/data/xiaolong/master_thesis/data/subset/val_subset.csv'
+        train_csv = '/data/xiaolong/master_thesis/data_preprocessing/subset/train_subset.csv'
+        val_csv = '/data/xiaolong/master_thesis/data_preprocessing/subset/val_subset.csv'
     else : 
-        train_csv = '/data/xiaolong/master_thesis/data/train_dataset.csv'
-        val_csv = '/data/xiaolong/master_thesis/data/val_dataset.csv'
+        train_csv = '/data/xiaolong/master_thesis/data_preprocessing/train_dataset.csv'
+        val_csv = '/data/xiaolong/master_thesis/data_preprocessing/val_dataset.csv'
         
     img_dir = '/data/xiaolong/rgb'
     dem_dir = '/data/xiaolong/dem'
     mask_dir = '/data/xiaolong/mask'
-    train_dataset = SwissImage(train_csv, img_dir, dem_dir, mask_dir, args.debug)
-    val_dataset = SwissImage(val_csv, img_dir, dem_dir, mask_dir, args.debug)
+    
+    img_transform = transforms.Compose([
+        transforms.RandomHorizontalFlip(p=0.5),
+        transforms.RandomVerticalFlip(p=0.5),
+        MyRandomRotation90(p=0.5),
+        transforms.ColorJitter(brightness=0.3, contrast=0.3, saturation=0.3, hue=0.05)
+        ])
+    
+    mask_transform = transforms.Compose([
+        transforms.RandomHorizontalFlip(p=0.5),
+        transforms.RandomVerticalFlip(p=0.5),
+        MyRandomRotation90(p=0.5),
+        ])
+    
+    train_dataset = SwissImage(train_csv, img_dir, dem_dir, mask_dir, transform=img_transform, mask_transform=mask_transform, debug=args.debug)
+    val_dataset = SwissImage(val_csv, img_dir, dem_dir, mask_dir, debug=args.debug)
     
     train_loader = DataLoader(
         train_dataset,
@@ -152,22 +172,6 @@ def main():
     best_perf = 0.0 # best performance so far (mean IoU)
     best_model = False
     start_epoch = 0
-    
-    #load existing model
-    if args.continues:
-        log_dir = '/data/xiaolong/master_thesis/out/checkpoint.pth.tar'
-        if os.path.exists(log_dir):
-            checkpoint = torch.load(log_dir)
-            model.load_state_dict(checkpoint['state_dict'])
-            optimizer.load_state_dict(checkpoint['optimizer'])
-            start_epoch = checkpoint['epoch']
-            perf_indicator = checkpoint['perf']
-            print('loadding epoch {} succeed'.format(start_epoch))
-        else:
-            print('loadding checkpoint failed, start trainning from epoch 0')
-            start_epoch = 0
-    else:
-        start_epoch = 0
 
     for epoch in range(start_epoch, args.epoch):
         # train for one epoch
@@ -189,18 +193,21 @@ def main():
             perf_indicator = -1
             best_model = False
 
+        lr_scheduler.step(perf_indicator)
+        
         # update best model so far
-        logger.info('=> saving checkpoint to {}'.format(args.out_dir))
+        folder_path = os.path.join(args.out_dir, folder_name)
+        logger.info('=> saving checkpoint to {}'.format(folder_path))
         save_checkpoint({
             'epoch': epoch + 1,
             'state_dict': model.state_dict(),
             'perf': perf_indicator,
             'last_epoch': epoch,
             'optimizer': optimizer.state_dict(),
-        }, best_model, args.out_dir)
+        }, best_model, folder_path)
 
 
-    final_model_state_file = os.path.join(args.out_dir,
+    final_model_state_file = os.path.join(folder_path,
                                           'final_state.pth.tar')
     logger.info('saving final model state to {}'.format(
         final_model_state_file))
