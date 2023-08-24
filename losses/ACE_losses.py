small fix on the backprop

losses/ACE_losses.py
@@ -105,7 +105,7 @@ class CELoss_3experts(nn.Module):
     def forward(self, output, targets):
         
         # Compute the classification loss for the head expert :
-        head_loss = self.celoss(output['exp_0'], targets)     
+        head_loss = self.celoss(output['exp_0'], targets)  .unsqueeze(0)   
         
         # Compute the classification loss from the body expert, only if there are pixel from body classes
         body_loss = torch.Tensor([0.]).to(self.device)

losses/aggregator_losses.py
@@ -27,14 +27,12 @@ class AggregatorLoss(nn.Module):
         
     def forward(self, output, targets):
         
-        loss = torch.Tensor([0.]).to(self.device)
+        loss = self.ce(output['aggregation'],targets)
         
         if  not self.finetune_classifier_only :
             
-            loss += torch.Tensor (self.expertLoss(output,targets)) .sum() 
-        
-        loss += self.ce(output['aggregation'],targets)
-        
-           
-        return loss
+           exp_losses = torch.cat (self.expertLoss(output,targets)  ) .sum()
+           loss = loss +exp_losses     
+             
+        return loss 
         