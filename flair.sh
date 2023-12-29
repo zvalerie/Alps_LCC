8e8a7f2aca5bbb51ec1ae37d806dc853288d17f9
zvalerie <valerie.zermatten@epfl.ch>
2023-12-29 10:45:49 +0100

    clean repo

--- flair.sh
+++ flair.sh
@@ -14,3 +14,19 @@ python train.py --name MCE2_dev  --experts 2   --lws True --L2penalty True --wei
 python train.py --name MCE3_dev --experts 3   --lws True --L2penalty True --weight_decay 0.2   --debug True   --log_wand True
 
 
+##################################################3333 
+
+#simple CE 
+python train.py --name CEL_dev  --experts 0  --loss celoss    --large_dataset True  --log_wand True
+
+# weighted CE
+python train.py --name WEL_dev   --experts 0    --loss inverse_freq_weights --large_dataset True  --log_wand True
+
+## SL
+python train.py --name SL_dev  --experts 0  --loss seesaw --large_dataset True  --log_wand True
+
+## MCE 2
+python train.py --name MCE2_dev  --experts 2   --lws True --L2penalty True --weight_decay 0.2    --large_dataset True  --log_wand True
+
+## MCE 3 
+python train.py --name MCE3_dev --experts 3   --lws True --L2penalty True --weight_decay 0.2   --large_dataset True  --log_wand True
